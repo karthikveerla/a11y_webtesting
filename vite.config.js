1e22d7013bb5e7aaa0d96750de70f457b897fbd8
Karthik Veerla <karthikveerla28@gmail.com>
2025-07-17 18:28:38 -0400

    added env

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
   const isGithub = mode === 'github';
   return {
     base: isGithub ? '/a11y-webtesting/' : '/',
