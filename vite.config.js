e322f6e683ac49edabee3fa79c8ca58d53ea4dbb
Karthik Veerla <karthikveerla28@gmail.com>
2025-07-17 18:24:53 -0400

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: '/a11y-webtesting/', // use your new repo name here
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const isGithub = mode === 'github';
+  return {
+    base: isGithub ? '/a11y-webtesting/' : '/',
+    plugins: [react()],
+  };
 });
