cc329ab0fa1df4e11afbe9b0db39aa001fe72198
Karthik Veerla <karthikveerla28@gmail.com>
2025-07-17 18:21:26 -0400

    updated repo name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/a11y-accessibility-demo/', // use your new repo name here
+  base: '/a11y-webtesting/', // use your new repo name here
   plugins: [react()],
 });
