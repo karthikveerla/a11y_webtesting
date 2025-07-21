05e1b19b07a4a3f4e40776855c56655e026847d7
Karthik Veerla <karthikveerla28@gmail.com>
2025-07-21 13:55:59 -0400

    preparing for vercel deployment

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/a11y_webtesting">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
