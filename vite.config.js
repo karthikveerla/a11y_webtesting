a093a19e0a341719cb7bdcd81dfd113bfb2c3562
Karthik Veerla <karthikveerla28@gmail.com>
2025-07-21 14:38:32 -0400

    updated base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/a11y_webtesting/', // must match your repo name
+  base: '/', 
   plugins: [react()],
 });
